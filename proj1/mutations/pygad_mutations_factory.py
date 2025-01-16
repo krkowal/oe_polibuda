59ffc4f0ceb1bcfbe14107eef59b40e3630df55b
Patryk Krzywdziński <51056034+krzywdziu@users.noreply.github.com>
2025-01-16 16:04:55 +0100

    Added mutations
    uniform and gaussian

--- proj1/mutations/pygad_mutations_factory.py
+++ proj1/mutations/pygad_mutations_factory.py
@@ -7,6 +7,23 @@ from proj1 import constants
 def one_point_mutation(offspring, ga_instance):
     pass
 
+def uniform_mutation(offspring, ga_instance):
+    for child in offspring:
+        position = np.random.randint(0, len(child))
+        child[position] = np.random.uniform(ga_instance.gene_space[position][0],
+                                             ga_instance.gene_space[position][1])
+    return offspring
+
+def gaussian_mutation(offspring, ga_instance):
+    for child in offspring:
+        for idx in range(len(child)):
+            while True:
+                normal_rand_value = np.random.normal(0, 1)
+                new_gene_value = child[idx] + normal_rand_value
+                if ga_instance.gene_space[idx][0] <= new_gene_value <= ga_instance.gene_space[idx][1]:
+                    child[idx] = new_gene_value
+                    break
+    return offspring
 
 class PygadMutationFactory:
     @staticmethod
@@ -19,8 +36,8 @@ class PygadMutationFactory:
             case constants.BOUNDARY:
                 return BoundaryMutation(mutation_param)
             case constants.UNIFORM:
-                return UniformMutation(mutation_param)
+                return uniform_mutation
             case constants.GAUSS:
-                return GaussMutation(mutation_param)
+                return gaussian_mutation
             case _:
                 raise ValueError(f"Unknown selection method: {name}")
